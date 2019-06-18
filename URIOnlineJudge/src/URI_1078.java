dd9834bb5fc667143dc109124dbf9fa205475fa4
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2019-06-17 21:07:21 -0300

    add URI_1078

---
+++ URIOnlineJudge/src/URI_1078.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI_1078 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			int produto = i * N;
+			System.out.println(i + " x " + N + " = " + produto);
+		}
+		sc.close();
+	}
+}
