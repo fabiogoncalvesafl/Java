8cba2ac79e5c71e1cc5d898a4fe566d8382d88fb
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2019-07-30 22:08:35 -0300

    add code

---
+++ Educando Web/src/entities/Order1.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.util.Date;
+
+import entities.enums.OrderStatus;
+
+public class Order1 {
+
+	private Integer id;
+	private Date moment;
+	private OrderStatus status;
+
+	public Order1() {
+	}
+
+	public Order1(Integer id, Date moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+	}
+}
