91ff045de6067decfa367379496d4a5161d6c915
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-02-05 21:39:33 -0300

    update

---
+++ curso_programacao/src/estruturaSequencial/EntradaDeDados1.java
@@ -0,0 +1,19 @@
+package estruturaSequencial;
+
+import java.util.Scanner;
+
+public class EntradaDeDados1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String x;
+		x = sc.next();
+		
+		System.out.println("Você digitou: " + x);
+		
+		sc.close();
+	}
+
+}
