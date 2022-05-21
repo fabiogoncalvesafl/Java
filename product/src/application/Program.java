6fd5b27fa2436be0d1b43500571188ad3090d730
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-05-21 18:25:15 -0300

    Update course Java

--- product/src/application/Program.java
+++ product/src/application/Program.java
@@ -12,14 +12,15 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		double price = sc.nextDouble();
+		//System.out.print("Quantity in stock: ");
+		//int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price);
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
--- product/src/entities/Product.java
+++ product/src/entities/Product.java
@@ -6,6 +6,20 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	public Product() {
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
 	public double totalValueInStock() {
 		return price * quantity;
 	}
