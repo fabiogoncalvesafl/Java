e190f19ef260a0d4c25123b8b8deb4c76a4ea25f
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-05-21 22:30:47 -0300

    Update Java

---
+++ account/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Account account;
+
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("Enter account holder: ");
+		String holder = sc.nextLine();
+
+		System.out.print("Is there na initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+
+		if (response == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			double inicialDeposit = sc.nextDouble();
+			account = new Account(number, holder, inicialDeposit);
+		} else {
+			account = new Account(number, holder);
+		}
+		
+		System.out.println();
+		System.out.println("Account data:");
+		System.out.println(account);
+
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data:");
+		System.out.println(account);
+
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data:");
+		System.out.println(account);
+
+		sc.close();
+	}
+
+}
