213fa6ecf3a4868b55806381dd6c8ea7ed953cdc
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-06-17 18:42:39 -0300

    Add post project

--- Curso Java/composicao/src/application/Program.java
+++ Curso Java/composicao/src/application/Program.java
@@ -28,8 +28,7 @@ public class Program {
 		String workerLevel = sc.nextLine();
 		System.out.print("Base salary: ");
 		double baseSalary = sc.nextDouble();
-		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,
-				new Department(departmentName));
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 
 		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
