b0d33079d254ba89b547292e62dad306c0619f44
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-06-21 22:17:53 -0300

    upcasting and downcasting

--- Curso Java/heranca-e-polimorfismo/src/application/Program.java
+++ Curso Java/heranca-e-polimorfismo/src/application/Program.java
@@ -1,15 +1,42 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
-		BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 0.0, 500.0);
-
-		System.out.println(account.getBalance());
-		System.out.println(account.getLoanLimit());
+		//UPCASTIG
+		
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		//DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.00);
+		
+		//BusinessAccount acc5 = (BusinessAccount)acc3; //Exception class entities.SavingsAccount cannot be cast to class entities.BusinessAccoun
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.00);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
+		
 	}
 
 }
