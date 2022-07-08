6b6f6959c6f2208b4da561eaa4a98bb182c494fd
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-07-08 17:35:54 -0300

    Add bloco-finally

---
+++ Curso Java/bloco-finally/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		File file = new File("//home//fabio//Documentos//in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
+	}
+
+}
